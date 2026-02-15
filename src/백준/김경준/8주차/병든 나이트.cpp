48162df55a7d74c4e5c5890dfe42608ae00a8fa2
KimKyungJun <1124kyungjun@naver.com>
2026-02-15 22:47:31 +0900

    백준 8주차 아줴줴이야

---
+++ src/백준/김경준/8주차/병든 나이트.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+int main()
+{
+    cin >> N >> M;
+
+    if (N == 1) cout << 1;
+    else if (N == 2) cout << min(4, (M + 1) / 2);
+    else if (M < 7) cout << min(4, M);
+    else cout << M - 2;
+
+    return 0;
+}
